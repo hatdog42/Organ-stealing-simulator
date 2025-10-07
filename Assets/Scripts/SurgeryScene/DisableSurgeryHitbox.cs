d6cd075a077c5d2edff2f36c9980b7c39c8cbd70
Emil20010 <mikkelHansenEmil@outlook.com>
2025-10-07 19:36:19 +0200

    Maze fix

--- Assets/Scripts/SurgeryScene/DisableSurgeryHitbox.cs
+++ Assets/Scripts/SurgeryScene/DisableSurgeryHitbox.cs
@@ -7,17 +7,20 @@ namespace SurgeryScene
     {
         [SerializeField] private Collider2D collider1;
         [SerializeField] private Collider2D collider2;
+        [SerializeField] private Collider2D collider3;
         
         private void OnEnable()
         {
             collider1.enabled = false;
             collider2.enabled = false;
+            collider3.enabled = false;
         }
 
         private void OnDisable()
         {
             collider1.enabled = true;
             collider2.enabled = true;
+            collider3.enabled = true;
         }
     }
 }
